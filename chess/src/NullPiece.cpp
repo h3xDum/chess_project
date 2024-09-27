647178d4e42b854695896822167c189383448125
h3xDum <hakmon.lior1@gmail.com>
2024-09-27 19:30:57 +0300

    updated structure

---
+++ chess/src/NullPiece.cpp
@@ -0,0 +1,11 @@
+#include "../include/NullPiece.h"
+
+NullPiece::NullPiece(int row, int col) : Piece(NULL, '#', row, col, NULL)
+{
+
+}
+
+bool NullPiece::isLegalMove(int row, int col) const
+{
+	return true;
+}
